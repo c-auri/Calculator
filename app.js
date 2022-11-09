f12fd705c5a1ac0f6d4db16f7ff20699d41968f1
c-auri <43008483+c-auri@users.noreply.github.com>
2022-11-09 09:01:20 +0100

    Add frontend

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide   = (a, b) => a / b
 
-function operate(operator, a, b) {
+export function operate(operator, a, b) {
     switch (operator) {
         case "+": return      add(a, b)
         case "-": return subtract(a, b)
