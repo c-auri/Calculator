8f00095a798b178a7dff707f299e45f4122b3c2a
c-auri <43008483+c-auri@users.noreply.github.com>
2022-11-09 08:07:15 +0100

    Implement operators

---
+++ app.js
@@ -0,0 +1,13 @@
+const add      = (a, b) => a + b
+const subtract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide   = (a, b) => a / b
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case "+": return      add(a, b)
+        case "-": return subtract(a, b)
+        case "×": return multiply(a, b)
+        case "÷": return   divide(a, b)
+    }
+}
