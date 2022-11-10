aad85e7e02f36468cb88631fd7643a5114a721f0
c-auri <43008483+c-auri@users.noreply.github.com>
2022-11-10 22:16:12 +0100

    Add clear functionality

--- js/app.js
+++ js/app.js
@@ -8,10 +8,12 @@ const input = document.querySelector('#input')
 const digitButtons = document.querySelectorAll('.digit')
 const operatorButtons = document.querySelectorAll('.operator')
 const equalsButton = document.querySelector('#EQ')
+const clearButton = document.querySelector('#C')
 
 digitButtons.forEach(btn => btn.addEventListener("click", e => appendDisplay(e.target.textContent)))
 operatorButtons.forEach(btn => btn.addEventListener("click", e => handleOperator(e.target.textContent)))
 equalsButton.addEventListener("click", solve)
+clearButton.addEventListener("click", clear)
 
 function handleOperator(symbol) {
     if (typeof operator === "undefined") {
@@ -31,6 +33,13 @@ function solve() {
     operand2 = undefined
     resetDisplay(`${result}`)
     setOperand1()
+}
+
+function clear() {
+    operator = undefined
+    operand1 = undefined
+    operand2 = undefined
+    resetDisplay("")
 }
 
 function setOperand1() {
