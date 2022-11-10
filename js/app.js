4c9e311a1bff367ce1f8201d9361ecd4baec5878
c-auri <43008483+c-auri@users.noreply.github.com>
2022-11-10 22:10:17 +0100

    Implement basic arithmetic

--- js/app.js
+++ js/app.js
@@ -1,12 +1,51 @@
-const input = document.querySelector('#input')
+import { operate } from "./math.js"
 
+let operator = undefined
+let operand1 = undefined
+let operand2 = undefined
+
+const input = document.querySelector('#input')
 const digitButtons = document.querySelectorAll('.digit')
 const operatorButtons = document.querySelectorAll('.operator')
-const inputButtons = document.querySelectorAll('.digit, .operator, #dot')
+const equalsButton = document.querySelector('#EQ')
 
-inputButtons.forEach(btn => btn.addEventListener("click", e => writeToDisplay(e.target.textContent)))
+digitButtons.forEach(btn => btn.addEventListener("click", e => appendDisplay(e.target.textContent)))
+operatorButtons.forEach(btn => btn.addEventListener("click", e => handleOperator(e.target.textContent)))
+equalsButton.addEventListener("click", solve)
 
-function writeToDisplay(character) {
-    input.value += character
+function handleOperator(symbol) {
+    if (typeof operator === "undefined") {
+        setOperand1()
+    } else {
+        solve()
+    }
+
+    operator = symbol
+    appendDisplay(` ${symbol} `)
+}
+
+function solve() {
+    setOperand2()
+    const result = operate(operator, operand1, operand2)
+    operator = undefined
+    operand2 = undefined
+    resetDisplay(`${result}`)
+    setOperand1()
+}
+
+function setOperand1() {
+    operand1 = parseInt(input.value)
+}
+
+function setOperand2() {
+    operand2 = parseInt(input.value.split(operator)[1])
+}
+
+function appendDisplay(symbol) {
+    input.value += symbol
     input.selectionStart = input.value.length
+}
+
+function resetDisplay(string) {
+    input.value = string
 }
